1fb874f0cf66c09e5fd4406d1fc1c9a468574f83
Sergei Kuznetsov <s8kuznetsov888@gmail.com>
2025-01-23 21:37:57 +0300

    fix data

--- database.sql
+++ database.sql
@@ -1,20 +1,8 @@
 DROP TABLE if exists urls CASCADE;
-DROP TABLE if exists url_checks CASCADE;
 
 
 CREATE TABLE urls (
     id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     name varchar UNIQUE NOT NULL,
-    created_at date
-);
-
-
-CREATE TABLE url_checks (
-    id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-    url_id bigint REFERENCES urls(id) ON DELETE CASCADE,
-    status_code bigint,
-    h1 varchar,
-    title varchar,
-    description varchar,
     created_at date
 );
