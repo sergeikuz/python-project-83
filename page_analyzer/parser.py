20775a0cfb79e5647503568dcbca1f8068810908
Sergei Kuznetsov <s8kuznetsov888@gmail.com>
2025-03-20 19:31:03 +0300

    fix length in parser values

--- page_analyzer/parser.py
+++ page_analyzer/parser.py
@@ -5,11 +5,12 @@ def get_data(content: str) -> dict:
     soup = BeautifulSoup(content, 'lxml')
     data = {}
     title = soup.title.text
-    data['title'] = title if title and len(title) <= 70 else None
-    data['h1'] = soup.h1.text if soup.h1 and len(soup.h1) <= 65 else None
+    data['title'] = title[0:69] if title else None
+    h1 = soup.h1.text
+    data['h1'] = h1[0:64] if h1 else None
     data['description'] = None
     meta = soup.find('meta', attrs={'name': 'description'})
-    if meta and len(meta) <= 300:  
-        data['description'] = meta.get('content', None)
+    if meta:  
+        data['description'] = meta.get('content', None)[0:299]
     
     return data
